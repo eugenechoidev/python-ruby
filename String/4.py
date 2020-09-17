8f3cf68346ad02f358ca38412c3ad6dc88743872
eugenechoidev <47966909+eugenechoidev@users.noreply.github.com>
2020-09-17 19:59:23 +0900

    Add files via upload

---
+++ String/4.py
@@ -0,0 +1,6 @@
+print("eugene's \"tutorial\"")
+print("\\")
+print("Hello\nworld")
+print("Hello\t\tworld")
+print("\a")
+print('Hello\nworld')
---
+++ String/4.rb
@@ -0,0 +1,6 @@
+puts("eugene's \"tutorial\"")
+puts("\\")
+puts("Hello\nworld")
+puts("Hello\t\tworld")
+puts("\a")
+puts('Hello\nworld')
