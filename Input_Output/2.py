2a12dc29091bf438dc87d36ab2085da8f02f2c52
eugenechoidev <47966909+eugenechoidev@users.noreply.github.com>
2020-09-21 13:38:31 +0900

    Add files via upload

---
+++ Input_Output/2.py
@@ -0,0 +1,9 @@
+in_str = input("아이디를 입력해주세요.\n")
+real_eugene = "11"
+real_choi = "ab"
+if real_eugene == in_str:
+  print("Hello!, eugene")
+elif real_choi == in_str:
+  print("Hello!, choi")
+else:
+  print("who are you?")
---
+++ Input_Output/2.rb
@@ -0,0 +1,11 @@
+puts("아이디를 입력해주세요")
+input = gets.chomp()
+real_eugene = "11"
+real_choi = "ab"
+if real_eugene == input
+  puts("Hello!, eugene")
+elsif real_choi == input
+  puts("Hello!, choi")
+else
+  puts("who are you?")
+end
