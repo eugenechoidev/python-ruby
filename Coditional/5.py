86f3607e9079bba8a12e1a75f25afb12fde2fe04
eugenechoidev <47966909+eugenechoidev@users.noreply.github.com>
2020-09-21 13:37:33 +0900

    Add files via upload

---
+++ Coditional/5.py
@@ -0,0 +1,9 @@
+in_str = 11
+real_eugene = 11
+real_choi = "ab"
+if real_eugene == in_str:
+  print("Hello!, eugene")
+elif real_choi == in_str:
+  print("Hello!, choi")
+else:
+  print("who are you?")
